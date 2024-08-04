255031b78ff0b83878b5007cfa9799b60cf6dd93
Lastonetry <alwaysoptimisticfutureiitian@gmail.com>
2024-08-04 07:41:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -62,10 +62,15 @@ def progress(current, total, message, type):
 
 # start command
 @bot.on_message(filters.command(["start"]))
-def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
-	await bot.send_message(message.chat.id, f"**__👋 Hi** **{message.from_user.mention}**, **I am Save Restricted Bot, I can send you restricted content by it's post link__**\n\n{USAGE}",
-	reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("🌐 Update Channel", url="https://t.me/VJ_Botz")]]), reply_to_message_id=message.id)
-
+async def send_start(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
+    await bot.send_message(
+        message.chat.id, 
+        f"👋 Hi {message.from_user.mention}, I am Save Restricted Bot, I can send you restricted content by it's post link\n\n{USAGE}",
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton("🌐 Update Channel", url="https://t.me/aHelperHand")]]
+        ), 
+        reply_to_message_id=message.id
+    )
 
 @bot.on_message(filters.text)
 def save(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
